08d67e7182a37ead77d310d91e4536f650e9e72e
Mike Kasberg <kasberg.mike@gmail.com>
2025-01-01 12:04:32 -0700

    Simplify to dpkg-deb

--- build-ghostty.sh
+++ build-ghostty.sh
@@ -4,12 +4,6 @@ set -e
 
 GHOSTTY_VERSION="1.0.1"
 
-DEBEMAIL="kasberg.mike@gmail.com"
-DEBFULLNAME="Mike Kasberg"
-DEBUILD_DPKG_BUILDPACKAGE_OPTS="-i -I -us -uc"
-DEBUILD_LINTIAN_OPTS="-i -I --show-overrides"
-DEB_BUILD_MAINT_OPTIONS="hardening=+all"
-
 # Fetch Ghostty Source
 wget -q "https://release.files.ghostty.org/$GHOSTTY_VERSION/ghostty-$GHOSTTY_VERSION.tar.gz"
 wget -q "https://release.files.ghostty.org/$GHOSTTY_VERSION/ghostty-$GHOSTTY_VERSION.tar.gz.minisig"
@@ -18,11 +12,31 @@ minisign -Vm "ghostty-$GHOSTTY_VERSION.tar.gz" -P RWQlAjJC23149WL2sEpT/l0QKy7hMI
 rm ghostty-$GHOSTTY_VERSION.tar.gz.minisig
 
 tar -xzmf "ghostty-$GHOSTTY_VERSION.tar.gz"
-ln -s "ghostty-$GHOSTTY_VERSION.tar.gz" "ghostty_$GHOSTTY_VERSION.orig.tar.gz"
 
-cp -r debian "ghostty-$GHOSTTY_VERSION/debian"
+cp -r DEBIAN/ "ghostty-$GHOSTTY_VERSION/DEBIAN/"
 
 # Build Ghostty
 cd "ghostty-$GHOSTTY_VERSION"
 
-debuild --prepend-path /usr/local/bin -us -uc $@
+# On Ubuntu it's libbz2, not libbzip2
+sed -i 's/linkSystemLibrary2("bzip2", dynamic_link_opts)/linkSystemLibrary2("bz2", dynamic_link_opts)/' build.zig
+
+# Fetch Zig Cache
+ZIG_GLOBAL_CACHE_DIR=/tmp/offline-cache ./nix/build-support/fetch-zig-cache.sh
+
+# Build Ghostty with zig
+zig build \
+  --summary all \
+  --prefix ./zig-out/usr \
+  --system /tmp/offline-cache/p \
+  -Doptimize=ReleaseFast \
+  -Dcpu=baseline \
+  -Dpie=true \
+  -Demit-docs \
+  -Dversion-string=$GHOSTTY_VERSION
+
+cd ..
+
+cp -r DEBIAN/ "ghostty-$GHOSTTY_VERSION/zig-out/DEBIAN/"
+
+dpkg-deb --build ghostty-$GHOSTTY_VERSION/zig-out
