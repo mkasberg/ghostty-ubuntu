83e5a10e620c18564a7edd01a14259851f0a8a9d
Mike Kasberg <kasberg.mike@gmail.com>
2025-01-01 07:02:36 -0700

    Install zig in configure step
    I can't currently put this binary package in a PPA because the PPA
    server builds from source itself. That's a problem because the PPA build
    server is missing zig. We can work around this with a little "hack".
    Install zig to /opt an symlink to /usr/local/bin as part of the
    "configure" step.
    
    Fixes #9

--- setup-env.sh
+++ setup-env.sh
@@ -5,7 +5,6 @@
 set -e
 
 DEBIAN_FRONTEND="noninteractive"
-ZIG_VERSION="0.13.0"
 
 # Install Build Tools
 apt-get -qq update && apt-get -qq -y install build-essential debhelper devscripts pandoc libonig-dev libbz2-dev wget
@@ -14,11 +13,6 @@ wget -q "https://github.com/jedisct1/minisign/releases/download/0.11/minisign-0.
 tar -xzf minisign-0.11-linux.tar.gz
 mv minisign-linux/x86_64/minisign /usr/local/bin
 rm -r minisign-linux
-
-wget -q "https://ziglang.org/download/$ZIG_VERSION/zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-tar -xf "zig-linux-x86_64-$ZIG_VERSION.tar.xz" -C /opt
-rm "zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-ln -s "/opt/zig-linux-x86_64-$ZIG_VERSION/zig" /usr/local/bin/zig
 
 # Install Ghostty Dependencies
 apt-get -qq -y install libgtk-4-dev libadwaita-1-dev
