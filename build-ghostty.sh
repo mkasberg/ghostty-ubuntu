ee8da4903fc58917b4c19ab2d501533544a3fdc7
Mike Kasberg <kasberg.mike@gmail.com>
2025-02-15 07:10:44 -0700

    Temporary Build Script Fix for 1.1.2
    Versions 1.1.1 and 1.1.2 are missing fetch-zig-cache.sh, we need to get
    it from version 1.1.0 for now.
    
    https://github.com/ghostty-org/ghostty/blob/f1f1120749b7494c89689d993d5a893c27c236a5/PACKAGING.md?plain=1#L84-L89

--- build-ghostty.sh
+++ build-ghostty.sh
@@ -19,6 +19,17 @@ rm ghostty-$GHOSTTY_VERSION.tar.gz.minisig
 
 tar -xzmf "ghostty-$GHOSTTY_VERSION.tar.gz"
 
+# Temporary Fix for 1.1.1 & 1.1.2: Get fetch-zig-cache.sh from 1.1.0
+# https://github.com/ghostty-org/ghostty/blob/f1f1120749b7494c89689d993d5a893c27c236a5/PACKAGING.md?plain=1#L84-L89
+wget -q "https://release.files.ghostty.org/1.1.0/ghostty-1.1.0.tar.gz"
+wget -q "https://release.files.ghostty.org/1.1.0/ghostty-1.1.0.tar.gz.minisig"
+minisign -Vm "ghostty-1.1.0.tar.gz" -P RWQlAjJC23149WL2sEpT/l0QKy7hMIFhYdQOFy0Z7z7PbneUgvlsnYcV
+rm ghostty-1.1.0.tar.gz.minisig
+tar -xzmf "ghostty-1.1.0.tar.gz"
+cp ghostty-1.1.0/nix/build-support/fetch-zig-cache.sh "ghostty-$GHOSTTY_VERSION/nix/build-support/"
+rm -r ghostty-1.1.0
+rm ghostty-1.1.0.tar.gz
+
 cd "ghostty-$GHOSTTY_VERSION"
 
 # On Ubuntu it's libbz2, not libbzip2
