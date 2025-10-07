3bb1b0467a3308549dc167a44f6d3931b217d0d4
Mike Kasberg <mkasberg@strava.com>
2025-10-06 21:59:11 -0700

    Fix version in build script

--- build-ghostty.sh
+++ build-ghostty.sh
@@ -13,7 +13,7 @@ fi
 DISTRO=$(lsb_release -sc)
 
 #FULL_VERSION="$GHOSTTY_VERSION-0~${DISTRO}1"
-FULL_VERSION="$GHOSTTY_VERSION-0~ppa3"
+FULL_VERSION="$GHOSTTY_VERSION-0~ppa1"
 
 echo "Fetch Ghostty Source"
 wget -q "https://release.files.ghostty.org/$GHOSTTY_VERSION/ghostty-$GHOSTTY_VERSION.tar.gz"
