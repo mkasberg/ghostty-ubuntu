882428bbc7272d303c0158d84e740ad3e7310133
Mike Kasberg <kasberg.mike@gmail.com>
2025-11-27 11:44:52 -0700

    Test with nightly build

--- fetch_ghostty_orig_source.sh
+++ fetch_ghostty_orig_source.sh
@@ -15,26 +15,40 @@ if [ -z "$VERSION" ]; then
   exit 1
 fi
 
+
 REPACK_SUFFIX="~vendor1"
-GHOSTTY_TARBALL="ghostty-${VERSION}.tar.gz"
-GHOSTTY_SIGNATURE="ghostty-${VERSION}.tar.gz.minisig"
 GHOSTTY_PUBKEY="RWQlAjJC23149WL2sEpT/l0QKy7hMIFhYdQOFy0Z7z7PbneUgvlsnYcV"
-GHOSTTY_DIR="ghostty-${VERSION}"
-REPACK_TARBALL="ghostty_${VERSION}${REPACK_SUFFIX}.orig.tar.gz"
+if [[ "$VERSION" == "tip" ]]; then
+  DATE=$(date -u +"%Y%m%d")
+  GHOSTTY_TARBALL="ghostty-source.tar.gz"
+  REPACK_TARBALL="ghostty_1.2.3+nightly${DATE}${REPACK_SUFFIX}.orig.tar.gz"
+  TARBALL_URL="https://github.com/ghostty-org/ghostty/releases/download/tip/ghostty-source.tar.gz"
+else
+  GHOSTTY_TARBALL="ghostty-${VERSION}.tar.gz"
+  GHOSTTY_DIR="ghostty-${VERSION}"
+  REPACK_TARBALL="ghostty_${VERSION}${REPACK_SUFFIX}.orig.tar.gz"
+  TARBALL_URL="https://release.files.ghostty.org/${VERSION}/${GHOSTTY_TARBALL}"
+fi
+GHOSTTY_SIGNATURE="${TARBALL_URL}.minisig"
+
 
 echo "Downloading ghostty source..."
-wget "https://release.files.ghostty.org/${VERSION}/${GHOSTTY_TARBALL}"
-wget "https://release.files.ghostty.org/${VERSION}/${GHOSTTY_SIGNATURE}"
+wget "$TARBALL_URL"
+wget "$GHOSTTY_SIGNATURE"
 
 echo "Verifying ghostty source..."
 minisign -V -P "${GHOSTTY_PUBKEY}" -m "${GHOSTTY_TARBALL}"
 
 echo "Extracting ghostty source..."
+if [ -z "$GHOSTTY_DIR" ]; then
+  GHOSTTY_DIR=$(tar -tzf ghostty-source.tar.gz | head -n1)
+fi
 tar -xzf "${GHOSTTY_TARBALL}"
 
 echo "Fetching zig cache..."
-# We need || true here because a theme release from 1.2.3 is failing...
-ZIG_GLOBAL_CACHE_DIR="${GHOSTTY_DIR}/vendor-zig-cache" "${GHOSTTY_DIR}/nix/build-support/fetch-zig-cache.sh" || true
+ZIG_GLOBAL_CACHE_DIR="${GHOSTTY_DIR}/vendor-zig-cache" "${GHOSTTY_DIR}/nix/build-support/fetch-zig-cache.sh"
+find "${GHOSTTY_DIR}/vendor-zig-cache" -name '*.exe' -delete
+find "${GHOSTTY_DIR}/vendor-zig-cache" -name '*.dll' -delete
 
 echo "Repacking ghostty source..."
 tar -czf "${REPACK_TARBALL}" "${GHOSTTY_DIR}"
