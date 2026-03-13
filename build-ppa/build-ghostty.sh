856acabed25adba6c5cef7d3f11c5c2f86357217
Mike Kasberg <kasberg.mike@gmail.com>
2026-03-13 11:39:02 -0600

    Bump to 1.3.1

--- build-ppa/build-ghostty.sh
+++ build-ppa/build-ghostty.sh
@@ -20,7 +20,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CODENAME="questing"
 SIGN_PACKAGE=false
 VERSION="tip"
-PPA_VERSION="ppa2"
+PPA_VERSION="ppa1"
 
 # Parse command line arguments
 while getopts 'hc:sv:' opt; do
